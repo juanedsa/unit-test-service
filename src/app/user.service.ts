fa86e37064ee1bf9496c7c20b02cecdd094f36c3
Juan Eduardo Salazar Arcila <salazaju@colpatria.com>
2019-02-07 16:35:42 -0500

    add user service

---
+++ src/app/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUser() {
+    return this.http.get(this.url);
+  }
+
+  getUserWithError() {
+    return this.http.get(this.url + 'bad');
+  }
+}
